4154f169c068f0181ab69653c86f33a77323ab06
Doreen Wiggins <Wiggy77@aol.com>
2018-01-08 22:45:00 -0500

    functions entered now debugging

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -10,62 +10,42 @@ var burger = require("../models/burger.js");
 router.get("/", function (req, res) {
   burger.all(function (data) {
 
-    // var burgerList = JSON.stringify(data);
-    
     var burgerList = {
       burgers: data
     }
-    //       var burgerList = [];
-    // for (i =0; i < burgers.data.length; i++) {
-      // burgerList.push(burgers[i].burger_name);  
-      // var burgerList = [];
-      // for (i=0; i < )     
-      console.log("data is: " + res);
-      console.log(burgerList);
+
+    console.log("data is: " + res);
+    console.log(burgerList);
     res.render("index", burgerList);
   });
 });
 
-// router.post("/api/cats", function(req, res) {
-//   cat.create([
-//     "name", "sleepy"
-//   ], [
-//     req.body.name, req.body.sleepy
-//   ], function(result) {
-//     // Send back the ID of the new quote
-//     res.json({ id: result.insertId });
-//   });
-// });
+router.post("/api/burgers", function (req, res) {
+  burger.create([
+    "name"
+  ], [
+    req.body.name,
+  ], function (result) {
+    res.json({
+      id: result.insertId
+    });
+  });
+});
 
-// router.put("/api/cats/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
+router.put("/api/burgers/:id", function (req, res) {
+  var id = req.params.id;
 
-//   console.log("condition", condition);
+  burger.updateOne(function (data) {
 
-//   cat.update({
-//     sleepy: req.body.sleepy
-//   }, condition, function(result) {
-//     if (result.changedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
+    var burgerList = {
+      burgers: data
+    }
+  });
+  console.log(burgerList);
+  res.render("index", burgerList);
 
-// router.delete("/api/cats/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
+});
 
-//   cat.delete(condition, function(result) {
-//     if (result.affectedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-// });
-// });
 
 
 
--- models/burger.js
+++ models/burger.js
@@ -6,37 +6,33 @@ var orm = require("../config/orm.js");
 
 
 var burger = {
-    all: function(cb) {
-      orm.selectAll("burgers", function(res) {
-        cb(res);
-        console.log("result of burger specific is: " + JSON.stringify(res));
-        
-      });
-    }
+  all: function (cb) {
+    orm.selectAll("burgers", function (res) {
+      cb(res);
+      console.log("result of burger specific is: " + JSON.stringify(res));
+
+    });
+  },
   //   // The variables cols and vals are arrays.
   //   create: function(cols, vals, cb) {
   //     orm.create("cats", cols, vals, function(res) {
   //       cb(res);
   //     });
   //   },
-  //   update: function(objColVals, condition, cb) {
-  //     orm.update("cats", objColVals, condition, function(res) {
-  //       cb(res);
-  //     });
-  //   },
-  //   delete: function(condition, cb) {
-  //     orm.delete("cats", condition, function(res) {
-  //       cb(res);
-  //     });
-  //   }
-  // };
-  
-  // Export the database functions for the controller 
+  update: function (tableInput, isSnack, id, cb) {
+    orm.updateOne("burgers", true, id, cb, function (res) {
+      cb(res);
+      console.log(JSON.stringify(res));
+    });
+  },
 
+  insert: function (tableInput, colName, colValue, cb) {
+    orm.insertOne("burgers", "burger_name", name, cb)
+    cb(res);
+    console.log(JSON.stringify(res));
+  }
 
-
-
-  };
+};
 
 
 
